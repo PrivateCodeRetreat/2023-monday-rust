8eddb08606662905a751cc60b9b5994902231a49
Lars Eckart <lars.eckart@hey.com>
2023-11-06 21:40:01 +0200

    board with 1 cell dies

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,36 @@ pub fn next_state() -> bool {
     true
 }
 
+pub struct Board {
+    pub cells: Vec<(i32, i32)>,
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board { cells: Vec::new() }
+    }
+    pub fn is_alive(&self, cell: (i32, i32)) -> bool {
+        self.cells.contains(&cell)
+    }
+
+    pub fn next_state(&mut self) -> Board {
+        let mut new_board = Board::new();
+        for cell in &self.cells {
+            let mut neighbors = 0;
+            for x in -1..2 {
+                for y in -1..2 {
+                    if x == 0 && y == 0 {
+                        continue;
+                    }
+                    if self.is_alive((cell.0 + x, cell.1 + y)) {
+                        neighbors += 1;
+                    }
+                }
+            }
+            if neighbors == 2 || neighbors == 3 {
+                new_board.cells.push(*cell);
+            }
+        }
+        new_board
+    }
+}
--- tests/hello-world.rs
+++ tests/hello-world.rs
@@ -1,7 +1,17 @@
 #![cfg_attr(feature = "strict", deny(warnings))]
 
-
+use hello_world::Board;
 #[test]
 fn die_from_underpopulation() {
-    assert_eq!(true, hello_world::next_state());
+    let mut board = Board::new();
+    // set cell at 1,1 alive
+    board.cells.push((1, 1));
+    // assert that the cell at 1,1 is alive
+    assert_eq!(true, board.is_alive((1,1)));
+    // call next_state()
+    board = board.next_state();
+    // assert that the cell at 1,1 is dead
+    assert_eq!(false, board.is_alive((1,1)));
+
+    
 }
